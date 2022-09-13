f5ad46adc2389be1ad2eeb07965bae19f5cb6ded
redengin <sholstei@yahoo.com>
2022-09-13 01:07:12 -0400

    loading firmware

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Bosch BMA421
 ================================================================================
-Developed per [Infintime](https://github.com/InfiniTimeOrg/InfiniTime/blob/develop/src/drivers/Bma421_C/bma4.c) source code.
+[Datasheet](https://files.pine64.org/doc/datasheet/pinetime/BST-BMA421-FL000.pdf)
+
+Note this part is not available to the public, and no public documentation is provided.
+
+This driver was implemented per the
+[Infintime](https://github.com/InfiniTimeOrg/InfiniTime/blob/develop/src/drivers/Bma421_C/bma4.c)
+source code.
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 
 use embedded_hal as hal;
 use num_enum::{TryFromPrimitive};
-use core::convert::TryFrom;
+use core::{convert::TryFrom, slice::Windows};
+mod firmware;
 
 pub struct BMA421<I2C, InterruptPin> {
     i2c: I2C,
@@ -53,12 +54,80 @@ impl<I2C, CommunicationError, InterruptPin> BMA421<I2C, InterruptPin>
 
         // verify chip id
         const BMA421_CHIP_ID:u8 = 0x11;
-        let chip_id = _self.read_register(Registers::CHIP_ID)?;
+        let chip_id = _self.read_register(Register::CHIP_ID)?;
         if chip_id != BMA421_CHIP_ID {
             return Err(Error::UnknownChipId(chip_id))
         }
 
+        // FIXME is this necessary upon init
+        let firmware = firmware::BLOB;
+        _self.load_firmware(&firmware)?;
+
         Ok(_self)
+    }
+
+    /// load a binary firmware blob into the chip
+    pub fn load_firmware(&mut self, firmware: &[u8]) -> Result<bool, Error<CommunicationError>> {
+        self.disable_adv_power_save()?;
+
+        // wait 450us for time synchronization
+        // TODO should this be an activity of disable_adv_power_save?
+        // FIXME should wait
+
+        // disable initialization
+        self.write_register(Register::INIT_CTRL, 0)?;
+
+        // write the binary blob
+        const BLOCK_LENGTH:usize = 16;
+        debug_assert_eq!(0, (firmware.len() % BLOCK_LENGTH), "firmware blob size doesn't match block size");
+        let blocks = firmware.len() / BLOCK_LENGTH;
+        for block in 0..blocks {
+            let index = block * BLOCK_LENGTH;
+            let index_msb = ((index / 2) >> 4) as u8;
+            let index_lsb = ((index / 2) as u8) & 0x0F;
+            self.write_register(Register::MAGIC_5B, index_lsb)?;
+            self.write_register(Register::MAGIC_5C, index_msb)?;
+            const REQUEST_SIZE:usize = 1 + BLOCK_LENGTH;
+            let mut request:[u8;REQUEST_SIZE] = [0;REQUEST_SIZE];
+            request[0] = Register::FEATURES_IN as u8;
+            request[1..REQUEST_SIZE].copy_from_slice(&firmware[index..index+BLOCK_LENGTH]);
+            self.i2c.write(self.address as u8, &request).map_err(Error::I2c)?;
+        } 
+
+        // wait for 150us to initialize the ASIC
+        // FIXME should wait
+
+        // confirm load
+        let status = self.read_register(Register::MAGIC_2A)?;
+        const ASIC_INITIALIZED_MASK:u8 = 0b1;
+        Ok((status & ASIC_INITIALIZED_MASK) == ASIC_INITIALIZED_MASK)
+    }
+
+    /// https://www.mouser.com/datasheet/2/783/BST-BMA423-DS000-1509600.pdf#%5B%7B%22num%22%3A242%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C61%2C442%2C0%5D
+    pub fn disable_adv_power_save(&mut self) -> Result<(), Error<CommunicationError>> {
+        self.write_register(Register::PWR_CONF, 0)
+    }
+
+    /// https://www.mouser.com/datasheet/2/783/BST-BMA423-DS000-1509600.pdf#%5B%7B%22num%22%3A242%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22XYZ%22%7D%2C61%2C442%2C0%5D
+    pub fn enable_adv_power_save(&mut self, fifo_self_wakeup: bool) -> Result<(), Error<CommunicationError>> {
+        const FIFO_SELF_WAKEUP_SHIFT:usize = 1;
+        let value:u8 = 0b1  // enable adv_power_save
+            // choose fifo_self_wakeup config
+            | (if fifo_self_wakeup { 1 } else {0}) << FIFO_SELF_WAKEUP_SHIFT;
+        self.write_register(Register::PWR_CONF, value)
+    }
+
+
+
+
+
+// ARCHIVED code
+//--------------------------------------------------------------------------------
+
+    /// put the chip into sleep
+    /// 
+    pub fn sleep(&mut self) -> Result<(), Error<CommunicationError>> {
+        self.write_register(Register::ACC_CONF, Mode::Normal as u8)
     }
 
     pub fn low_power(&mut self, config: Option<LowPowerOverSamplingConfig>) -> Result<(), Error<CommunicationError>> {
@@ -73,30 +142,26 @@ impl<I2C, CommunicationError, InterruptPin> BMA421<I2C, InterruptPin>
                 None => 0
             };
 
-        self.set_mode(mode)
-    }
-
-    fn set_mode(&mut self, mode: u8) -> Result<(), Error<CommunicationError>> {
-        self.write_register(Registers::ACC_CONF, mode)
+        self.write_register(Register::ACC_CONF, mode)
     }
 
 
     fn drdy(&mut self) -> Result<bool, Error<CommunicationError>> {
-        let status = self.read_register(Registers::STATUS)?;
+        let status = self.read_register(Register::STATUS)?;
         const DRDY_MASK:u8 = 1 << 7;
         Ok((status & DRDY_MASK) == DRDY_MASK)
     }
 
     pub fn accelerations(&mut self) -> Result<[i16;3], Error<CommunicationError>> {
+#[cfg(debug_assertions)]
         // make sure the data is available
-    #[cfg(debug_assertions)]
         if !(self.drdy()?) {
             return Err(Error::DataNotReady)
         }
 
         // get the data
         let mut buffer:[u8;6] = [0;6];
-        self.read_registers(Registers::DATA_START, &mut buffer)?;
+        self.read_registers(Register::DATA_START, &mut buffer)?;
 
         // convert the data
         let mut accelerations:[i16;3] = [0; 3];
@@ -110,20 +175,20 @@ impl<I2C, CommunicationError, InterruptPin> BMA421<I2C, InterruptPin>
         Ok(accelerations)
     }
 
-    fn read_register(&mut self, register: Registers ) -> Result<u8, Error<CommunicationError>> {
+    fn read_register(&mut self, register: Register ) -> Result<u8, Error<CommunicationError>> {
         let request = &[register as u8];
         let mut response:[u8;1] = [0;1];
         self.i2c.write_read(self.address as u8, request, &mut response).map_err(Error::I2c)?;
         Ok(response[0])
     }
 
-    fn read_registers(&mut self, first_register: Registers, response: &mut [u8] ) -> Result<(), Error<CommunicationError>> {
+    fn read_registers(&mut self, first_register: Register, response: &mut [u8] ) -> Result<(), Error<CommunicationError>> {
         let request = &[first_register as u8];
         self.i2c.write_read(self.address as u8, request, response).map_err(Error::I2c)?;
         Ok(())
     }
 
-    fn write_register(&mut self, register: Registers, value: u8 ) -> Result<(), Error<CommunicationError>> {
+    fn write_register(&mut self, register: Register, value: u8 ) -> Result<(), Error<CommunicationError>> {
         let request = &[register as u8, value];
         self.i2c.write(self.address as u8, request).map_err(Error::I2c)?;
         Ok(())
@@ -162,7 +227,7 @@ pub enum LowPowerOverSamplingConfig {
 #[allow(non_camel_case_types)]
 #[repr(u8)]
 /// https://github.com/InfiniTimeOrg/InfiniTime/blob/develop/src/drivers/Bma421_C/bma4_defs.h#L105
-enum Registers {
+enum Register {
     CHIP_ID             = 0x00,
     ERR_REG             = 0x02,
     STATUS              = 0x03,
@@ -178,6 +243,7 @@ enum Registers {
     FIFO_LENGTH_START   = 0x24,
     FIFO_LENGTH_END     = 0x25,
     FIFO_DATA           = 0x26,
+    MAGIC_2A            = 0x2A,
     ACC_CONF            = 0x40,
     ACC_RANGE           = 0x41,
     AUX_CONF            = 0x44,
@@ -198,6 +264,11 @@ enum Registers {
     INT2_MAP            = 0x57,
     INT_MAP_DATA        = 0x58,
     INIT_CTRL           = 0x59,
+    /// used to program firmware
+    MAGIC_5B            = 0x5B,
+    /// used to program firmware
+    MAGIC_5C            = 0x5C,
+    /// magic burst writable address
     FEATURES_IN         = 0x5E,
     INTERNAL_ERROR      = 0x5F,
     IF_CONF             = 0x6B,
